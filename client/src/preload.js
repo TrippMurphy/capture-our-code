83a69b8200fe9079ee24d89a21cdb3bb3685b524
Tripp Murphy <trippmurphy@tripps-mbp.lan>
2024-04-21 01:46:17 -0400

    added vanilla frontend to electron, edits made to readme

---
+++ client/src/preload.js
@@ -0,0 +1,8 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('myAPI', {
+  desktop: true,
+  nodeIntegration: true
+})
